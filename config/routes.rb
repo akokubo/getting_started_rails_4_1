a9d4662601ae0f00d126fc911200a68f916dcae5
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2014-08-23 16:09:39 +0900

    サインイン/サインアウト/サインアップの実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   end
 
   resources :users
-  match '/sign_up', to: 'users#new', via: 'get'
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/sign_up',  to: 'users#new',        via: 'get'
+  match '/sign_in',  to: 'sessions#new',     via: 'get'
+  match '/sign_out', to: 'sessions#destroy', via: 'delete'
 
   root 'welcome#index'
 end
